1d2eb3dd408e454208c091651245df05bb0b720f
Thulasi <thulasipanndeshwara02@gmail.com>
2025-02-16 11:59:57 +0530

    done1

---
+++ demos/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UserList from "./component/UserList";
+
+const App = () => (
+  <div>
+    <h1>All Users</h1>
+    <UserList />
+  </div>
+);
+
+export default App;
---
+++ demos/src/component/UserList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
